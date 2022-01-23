049c10633ad2cfd372d76e90beaf0ecd48f0e7c3
Miqhtie <miqhtiedev@gmail.com>
2022-01-22 23:22:20 -0500

    feat: database models

--- src/models/Settings.ts
+++ src/models/Settings.ts
@@ -1,12 +1,32 @@
 import { Schema, model, Document } from "mongoose";
 import { Snowflake } from "discord.js";
 
+interface ICharacter {
+  id: string;
+  ownerId: string;
+  avatarURL: string;
+  name: string;
+  stats: {
+    health: number;
+    intelligence: number;
+    speed: number;
+    strength: number;
+  };
+}
+
+interface IWorld {
+  id: string;
+  characters: ICharacter[];
+}
+
 export interface ISettings extends Document {
   id: Snowflake;
+  worlds: IWorld[];
 }
 
 const SettingsSchema = new Schema({
   id: { type: String, requried: true, index: true, unique: true },
+  worlds: { type: [] },
 });
 
 export default model<ISettings>("settings", SettingsSchema);
