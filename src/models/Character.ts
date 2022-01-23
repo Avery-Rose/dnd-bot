3f3319bfc4b080b6b6034c7508e472c98b78bc80
Ryan Arora <arora.ryan@gmail.com>
2022-01-23 01:23:20 -0500

    refactor: refactor the collections in Options into their own files

---
+++ src/models/Character.ts
@@ -0,0 +1,28 @@
+import { Schema, model, Document } from "mongoose";
+import { Snowflake } from "discord.js";
+
+export interface ICharacter extends Document {
+  ownerId: Snowflake;
+  avatarUrl: string;
+  name: string;
+  stats: {
+    health: number;
+    intelligence: number;
+    speed: number;
+    strength: number;
+  };
+}
+
+const CharacterSchema = new Schema({
+  ownerId: { type: String, required: true, unique: true },
+  avatarUrl: { type: String, required: true },
+  name: { type: String, required: true },
+  stats: {
+    health: { type: Number, required: true },
+    intelligence: { type: Number, required: true },
+    speed: { type: Number, required: true },
+    strength: { type: Number, required: true },
+  },
+});
+
+export default model<ICharacter>("characters", CharacterSchema);
